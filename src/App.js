9410d579f60c203a4311095bffae4d599a50c307
Vlad Sotskyi <vlad@sotsky.com>
2024-09-01 14:58:51 +0200

    Final version

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   const { currentUser } = useAuth();
 
   return (
-    <Router>
+    <Router basename="/Wishlist-with-profile">
       <ToastContainer />
       <div className="App">
         <NavBar />
--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAH3nxWojwl_gEZtJSPQwI14Eu19Bl220Y",
-  authDomain: "wishlist-with-profile.firebaseapp.com",
-  projectId: "wishlist-with-profile",
-  storageBucket: "wishlist-with-profile.appspot.com",
-  messagingSenderId: "238040308220",
-  appId: "1:238040308220:web:251d6b7fef2813dbb7ba79",
-  measurementId: "G-NZNXFFZSX9",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
